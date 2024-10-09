dcb194b7c66c9d5ab7d6a669b4874b0cbc53af4b
CoreBinusAso <core.binusaso@gmail.com>
2024-10-09 22:05:55 +0700

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Set Firebase hosting targets
-firebase target:apply hosting kirai1 kirai1
-firebase target:apply hosting kirai2 kirai2
+firebase target:apply hosting site1 kirai1
+firebase target:apply hosting site2 kirai2
 
 # Deploy to Firebase Hosting
-firebase deploy --only hosting:kirai1
-firebase deploy --only hosting:kirai2
+firebase deploy --only hosting:site1
+firebase deploy --only hosting:site2
 
